3f0197d9263a4e0a41f3c05d428fdef5968f91f6
Oscar Neira <oneirag@yahoo.es>
2026-03-12 04:52:07 +0000

    Adds flash_attention_2 to transformers_serve

--- transformers_serve.sh
+++ transformers_serve.sh
@@ -1,1 +1,1 @@
-transformers serve --trust-remote-code --host 0.0.0.0 --continuous-batching --dtype bfloat16
+transformers serve --trust-remote-code --host 0.0.0.0 --continuous-batching --dtype bfloat16 --attn-implementation "flash_attention_2"
